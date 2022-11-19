79dcadf0eb1900a1b6946146fde5cba341e6155f
happylogs <happylogs@users.noreply.github.com>
2022-11-19 14:19:11 -0500

    Added input loop (testing) 2

--- src/ListenThread.java
+++ src/ListenThread.java
@@ -3,12 +3,14 @@ import java.io.ObjectInputStream;
 
 public class ListenThread extends Thread {
     @Override public void run() {
+        System.out.println("Initial run");
         while(true) {
             try {
                 ObjectInputStream ois = new ObjectInputStream(Main.endpoint.getInputStream());
                 int y = ois.readInt();
                 int x = ois.readInt();
 
+                System.out.println("Hello, world!");
                 System.out.println(y);
                 System.out.println(x);
             } catch (IOException e) {
